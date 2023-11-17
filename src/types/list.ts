a505f447e3b3a0102e79c6a9e95be338ceabd026
HWANBINYOO <dngh0825@naver.com>
2023-11-18 02:40:23 +0900

    Fix : PropertiesProps

--- src/types/list.ts
+++ src/types/list.ts
@@ -1,67 +1,70 @@
 export interface listProps {
-    cover: CoverProps
-    properties:PropertiesProps
+  cover: CoverProps;
+  properties: PropertiesProps;
 }
 
 export interface PropertiesProps {
-    Grade:{
-        multi_select: MultiSelectProps[]
-    },
-    Review:{
-        rich_text:TextProps[]
-    },
-    Described:{
-        rich_text:TextProps[]
-    },
-    Date:{
-        date:{
-            start:string,
-            end:string,
-        }
-    },
-    Ost:{
-        url:string
-    },
-    Category:{
-        multi_select: MultiSelectProps[]
-    },
-    Tag:{
-        select:{
-            name:string,
-            color:string,
-        }
-    },
-    Name:{
-        title:TextProps[]
-    },
-    FamousLine:{
-        rich_text:TextProps[]
-    }
+  Grade: {
+    multi_select: MultiSelectProps[];
+  };
+  Described: {
+    rich_text: TextProps[];
+  };
+  Date: {
+    date: {
+      start: string;
+      end: string;
+    };
+  };
+  Ost: {
+    url: string;
+  };
+  WatchaPedia: {
+    url: string;
+  };
+  Trailer: {
+    url: string;
+  };
 
+  Category: {
+    multi_select: MultiSelectProps[];
+  };
+  Tag: {
+    select: {
+      name: string;
+      color: string;
+    };
+  };
+  Name: {
+    title: TextProps[];
+  };
+  FamousLine: {
+    rich_text: TextProps[];
+  };
 }
 
 export interface TextProps {
-        text:{
-            content:string
-        }
+  text: {
+    content: string;
+  };
 }
 export interface MultiSelectProps {
-        color:string,
-        id:string,
-        name:string,
+  color: string;
+  id: string;
+  name: string;
 }
 export interface CoverProps {
-    external : {
-        url:string
-    },
-    file? : {
-        url:string
-    }
+  external: {
+    url: string;
+  };
+  file?: {
+    url: string;
+  };
 }
 
 export interface FilterQeuryType {
-    property: string,
-    multi_select:{
-        contains:string,
-    }
+  property: string;
+  multi_select: {
+    contains: string;
+  };
 }
