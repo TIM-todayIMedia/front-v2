689418aa6c0a92164836f9568acfd8f1dc84e348
HWANBINYOO <dngh0825@naver.com>
2023-11-23 21:08:50 +0900

    Fix : HomePage route

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,15 @@
 import Home from "@/components/Home";
 import { listProps } from "@/types";
 import CustomAxios from "@/utils/lib/CustomAxios";
+import { Suspense } from "react";
+import HomeLoadingPage from "./loading";
 
 export default async function HomePage() {
   const { data } = await CustomAxios.post("");
   const list: listProps[] = data.results;
-  return <Home list={list} />;
+  return (
+    <Suspense fallback={<HomeLoadingPage />}>
+      <Home list={list} />
+    </Suspense>
+  );
 }
