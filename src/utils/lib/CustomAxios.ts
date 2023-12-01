a670bba52a2e2a9e09e1d328b00a781420a704f5
HWANBINYOO <dngh0825@naver.com>
2023-12-01 14:05:00 +0900

    Fix : Cache-Control setting

--- src/utils/lib/CustomAxios.ts
+++ src/utils/lib/CustomAxios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const CustomAxios = axios.create({
   baseURL: `https://api.notion.com/v1/databases/${process.env.NEXT_PUBLIC_NOTION_DATABASE_ID}/query`,
   headers: {
+    "Cache-Control": "no-cache",
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,OPTIONS,PATCH",
     Accept: "application/json",
     "Notion-Version": "2022-06-28",
