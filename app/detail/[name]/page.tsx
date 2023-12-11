ec03a2049eb55de56a6390935973b2f3e065369c
HWANBINYOO <dngh0825@naver.com>
2023-12-11 14:27:32 +0900

    Fix : detail page url

--- app/detail/[name]/page.tsx
+++ app/detail/[name]/page.tsx
@@ -1,16 +1,15 @@
-import CustomAxios from "@/utils/lib/CustomAxios";
 import HomeDetail from "@/components/HomeDetail";
 import { Props } from "@/types/common";
 import { decodeParams } from "@/utils/decodeParams";
 import { Metadata } from "next";
-import { listProps } from "@/types";
+import { getDetailData } from "@/api/list";
 
 export const metadata: Metadata = {
   title: "Detail",
 };
 
 export const DetailPage = async ({ params: { name } }: Props) => {
-  const detailData = await getPost(decodeParams(name));
+  const detailData = await getDetailData(decodeParams(name));
   return <HomeDetail data={detailData} />;
 };
 
@@ -22,23 +21,5 @@ export const DetailPage = async ({ params: { name } }: Props) => {
 //     name: i.properties.Name.title[0].text.content,
 //   }));
 // }
-
-export const getPost = async (name: string) => {
-  try {
-    const { data } = await CustomAxios.post("", {
-      filter: {
-        property: "Name",
-        title: {
-          equals: name,
-        },
-      },
-    });
-    const detailData = data.results[0];
-    return detailData;
-  } catch (e) {
-    console.log(e);
-    return;
-  }
-};
 
 export default DetailPage;
