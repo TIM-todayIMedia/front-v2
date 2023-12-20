8f1c97a0a2a874cb6b452d226689f62ee1d926da
HWANBINYOO <dngh0825@naver.com>
2023-12-20 16:23:59 +0900

    Fix : Absolute Path Import

--- src/app/ui/loading/home/index.tsx
+++ src/app/ui/loading/home/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as S from "@/components/common/Styles/home";
-import SkeletonListBox from "../../Skeletons/SkeletonListBox";
+import SkeletonListBox from "@/app/ui/Skeletons/SkeletonListBox";
 
 export default function HomeLoadingUi() {
   const MockData: readonly number[] = Array.from(Array(10));
