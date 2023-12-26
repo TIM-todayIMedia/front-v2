a3e5170ff83a2d469e0b11fececfeb30bdfadce9
HWANBINYOO <dngh0825@naver.com>
2023-12-26 09:22:53 +0900

    👽️ Update : Promotion animation

--- src/components/Promotion/index.tsx
+++ src/components/Promotion/index.tsx
@@ -1,13 +1,66 @@
 "use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import CategorySection from "../Section/CategorySection";
+import TextBox from "../Section/TextBox";
 import * as S from "./styled";
 
 const Promotion = () => {
+  const router = useRouter();
+  const [visibleValue, setVisibleValue] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      console.log(scrollY);
+      if (scrollY < 101) {
+        setVisibleValue(1 - scrollY * 0.01);
+      } else if (scrollY > 100) {
+        setVisibleValue(0);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <S.PromotionWrapper>
       <S.Video loop muted autoPlay playsInline>
         <source src="/videos/demo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </S.Video>
+      <S.ScrollSection>
+        <S.MainTextBox style={{ opacity: visibleValue }}>
+          <S.MainText>
+            WTM 에서 제공하는{" "}
+            <span
+              style={{
+                color: "#3b3681",
+              }}
+            >
+              오늘의 영화/드라마
+            </span>
+            와 <br /> 다양한 정보들.
+          </S.MainText>
+          <S.DirectBtn onClick={() => router.push("/home")}>
+            지금 둘러보기
+          </S.DirectBtn>
+        </S.MainTextBox>
+        <S.ScrollBox>
+          <TextBox
+            title={
+              "지속적으로 업데이트되고있는 영화/드라마의 다양한 정보를 확인하세요"
+            }
+            desc={
+              "(제목, 설명, 장르, 평점, 명대사, 개봉일/상영일, 포스터, 예고편, 왓차피디아, Ost)"
+            }
+          />
+          <CategorySection />
+        </S.ScrollBox>
+      </S.ScrollSection>
     </S.PromotionWrapper>
   );
 };
--- src/components/Promotion/styled.ts
+++ src/components/Promotion/styled.ts
@@ -2,14 +2,78 @@ import styled from "@emotion/styled";
 
 export const PromotionWrapper = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Video = styled.video`
-  width: 100.1%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 102%;
   object-fit: cover;
   object-position: center top;
   outline: none;
   border: none;
+`;
+
+export const ScrollSection = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
+
+export const MainText = styled.div`
+  color: white;
+  z-index: 10;
+  font-weight: 900;
+  font-size: 80px;
+  text-align: center;
+`;
+
+export const MainTextBox = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const DirectBtn = styled.button`
+  width: 200px;
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+  font-weight: 800;
+  color: white;
+  z-index: 2;
+  border: none;
+  border-radius: 20px;
+  transition: all 0.2s;
+  background-color: #3b3681;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+
+  :hover {
+    height: 93px;
+    width: 240px;
+    font-size: 30px;
+  }
+`;
+
+export const ScrollBox = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  gap: 500px;
+  padding-bottom: 300px;
 `;
