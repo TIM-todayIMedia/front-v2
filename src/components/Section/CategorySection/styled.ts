a229316186bbc36d8ec97115f65ea97da901c517
HWANBINYOO <dngh0825@naver.com>
2023-12-27 11:34:51 +0900

    🔨 Add : CategorySection category 기능구현

--- src/components/Section/CategorySection/styled.ts
+++ src/components/Section/CategorySection/styled.ts
@@ -33,7 +33,7 @@ export const CategoryArrayWrapper = styled.div`
 
 export const PromotionCategoryItem = styled.div`
   width: 150px;
-  height: 85px;
+  height: 75px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -42,5 +42,34 @@ export const PromotionCategoryItem = styled.div`
   font-weight: 600;
   font-size: 30px;
   border-radius: 15px;
-  transition: all 0.3s ease-in;
+  transition: all 0.3s;
+  cursor: pointer;
+
+  :hover {
+    font-size: 32px;
+  }
+`;
+
+export const DirectBtn = styled.button`
+  width: 30%;
+  height: 75px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 30px;
+  font-weight: 800;
+  color: white;
+  z-index: 2;
+  border: none;
+  border-radius: 20px;
+  transition: all 0.2s;
+  background-color: #4940a4;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  margin: 20px 0;
+
+  :hover {
+    width: 40%;
+    height: 95px;
+    font-size: 35px;
+  }
 `;
