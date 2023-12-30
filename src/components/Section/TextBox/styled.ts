e7696d55dad7019922a2300dc4990be33454dfab
HWANBINYOO <dngh0825@naver.com>
2023-12-30 15:01:15 +0900

    💄 Add : TextBox Responsive

--- src/components/Section/TextBox/styled.ts
+++ src/components/Section/TextBox/styled.ts
@@ -8,15 +8,25 @@ export const TextBoxWrapper = styled.div`
   z-index: 10;
   transition: all 0.5s;
   margin-bottom: 350px;
+  word-break: keep-all;
 `;
 
 export const TextBoxTitle = styled.span`
   font-weight: 900;
   font-size: 71px;
   text-align: center;
+
+  @media (max-width: 763px) {
+    font-size: 35px;
+  }
 `;
 
 export const TextBoxDesc = styled.span`
   font-weight: 500;
   font-size: 27px;
+  text-align: center;
+
+  @media (max-width: 763px) {
+    font-size: 15px;
+  }
 `;
