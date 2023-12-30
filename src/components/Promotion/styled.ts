01e141c0033ed3f84dbeda7b4005a70fc509bf5c
HWANBINYOO <dngh0825@naver.com>
2023-12-30 15:00:13 +0900

    💄 Add : promotion responsive

--- src/components/Promotion/styled.ts
+++ src/components/Promotion/styled.ts
@@ -11,9 +11,9 @@ export const Video = styled.video`
   top: 0;
   height: 100%;
   object-fit: cover;
+  object-position: center top;
   outline: none;
   border: none;
-  overflow-y: none;
 `;
 
 export const ScrollSection = styled.div`
@@ -31,6 +31,11 @@ export const MainText = styled.div`
   font-weight: 900;
   font-size: 80px;
   text-align: center;
+  word-break: keep-all;
+
+  @media (max-width: 763px) {
+    font-size: 40px;
+  }
 `;
 
 export const MainTextBox = styled.div`
@@ -42,7 +47,7 @@ export const MainTextBox = styled.div`
   align-items: center;
   gap: 20px;
   margin: 0 auto;
-  overflow-y: scroll;
+  overflow-y: none;
 `;
 
 export const DirectBtn = styled.button`
@@ -65,6 +70,13 @@ export const DirectBtn = styled.button`
     height: 85px;
     width: 220px;
     font-size: 30px;
+  }
+
+  @media (max-width: 763px) {
+    width: 150px;
+    height: 50px;
+    font-size: 15px;
+    border-radius: 10px;
   }
 `;
 
