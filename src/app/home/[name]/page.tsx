ed145cc25505a5f446de65e77233f83af52164c5
HWANBINYOO <dngh0825@naver.com>
2024-02-13 04:30:35 +0900

    Fix : HomePage

--- src/app/home/[name]/page.tsx
+++ src/app/home/[name]/page.tsx
@@ -1,16 +1,8 @@
-import { getAllList, notionAllData } from "@/api/list";
+import { notionAllData } from "@/api/list";
 import Home from "@/components/Home";
-import { listProps } from "@/types";
-import { Props } from "@/types/common";
-import { decodeParams } from "@/utils/decodeParams";
-import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
-export default async function HomePage({ params: { name } }: Props) {
-  const list: listProps[] = await getAllList(decodeParams(name));
-  const list2 = await notionAllData();
+export default async function HomePage() {
+  const list = await notionAllData();
 
-  // console.log(list);
-  console.log(list2);
-
-  return <Home initList={list2} />;
+  return <Home initList={list} />;
 }
