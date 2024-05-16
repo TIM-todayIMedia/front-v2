9bd70229ee7bf5b5866a85f09d42de2b16675367
yoohwanbin <hwanbin@infludeo.com>
2024-05-16 17:59:56 +0900

    Fix : categorySection

--- src/components/Section/CategorySection/index.tsx
+++ src/components/Section/CategorySection/index.tsx
@@ -1,38 +1,38 @@
-import { useVisible } from "@/hooks/useVisible";
-import { CategoryArray } from "@/utils/CategoryArray";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import * as S from "./styled";
+import { useVisible } from '@/hooks/useVisible'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import * as S from './styled'
+import { CategoryItems } from 'wtm-api'
 
 const CategorySection = () => {
-  const router = useRouter();
-  const [ref, visible] = useVisible();
-  const [filterCategoryArray, setFilterCategoryArray] = useState<string[]>([]);
+  const router = useRouter()
+  const [ref, visible] = useVisible()
+  const [filterCategoryArray, setFilterCategoryArray] = useState<string[]>([])
 
   const handleCategorySelectClick = (name: string) => {
     if (!filterCategoryArray.includes(name)) {
-      setFilterCategoryArray([...filterCategoryArray, name]);
+      setFilterCategoryArray([...filterCategoryArray, name])
     } else {
-      const arr = filterCategoryArray.filter((i) => i !== name);
-      setFilterCategoryArray(arr);
+      const arr = filterCategoryArray.filter(i => i !== name)
+      setFilterCategoryArray(arr)
     }
-  };
+  }
 
   const handleSubmitClick = () =>
-    router.push(`/filter/${filterCategoryArray.join(" ")}`);
+    router.push(`/filter/${filterCategoryArray.join(' ')}`)
 
   return (
     <S.CategorySectionWrapper ref={ref}>
       <S.CategorySectionTitle
         style={{
           opacity: visible ? 1 : 0,
-          transform: visible ? "translateY(0px)" : "translateY(50px)",
+          transform: visible ? 'translateY(0px)' : 'translateY(50px)',
         }}
       >
         <span
           style={{
-            color: filterCategoryArray.length > 0 ? "white" : "#4940a4",
-            transition: "all 0.3s ease",
+            color: filterCategoryArray.length > 0 ? 'white' : '#4940a4',
+            transition: 'all 0.3s ease',
           }}
         >
           여러 장르
@@ -42,25 +42,25 @@ const CategorySection = () => {
       <S.CategorySectionDesc
         style={{
           opacity: visible ? 1 : 0,
-          transform: visible ? "translateY(0px)" : "translateY(50px)",
+          transform: visible ? 'translateY(0px)' : 'translateY(50px)',
         }}
       >
         여러가지 장르를 동시에 선택가능해 더 보고싶은 영화/미디어를 찾으실 수
         있습니다
       </S.CategorySectionDesc>
       <S.CategoryArrayWrapper>
-        {CategoryArray.map((category, idx) => (
+        {CategoryItems.map((category, idx) => (
           <S.PromotionCategoryItem
             key={idx}
             onClick={() => handleCategorySelectClick(category)}
             style={{
               backgroundColor: filterCategoryArray.includes(category)
-                ? "#4940a4"
-                : "white",
-              color: filterCategoryArray.includes(category) ? "white" : "gray",
+                ? '#4940a4'
+                : 'white',
+              color: filterCategoryArray.includes(category) ? 'white' : 'gray',
               opacity: visible ? 1 : 0,
               transform: visible
-                ? "translateY(0px) translateX(0px)"
+                ? 'translateY(0px) translateX(0px)'
                 : `translateY(${
                     idx + 1 > 7 ? (idx + 1) * -50 : (idx + 1) * +50
                   }px) translateX(${
@@ -77,13 +77,13 @@ const CategorySection = () => {
           opacity: filterCategoryArray.length > 0 ? 1 : 0,
         }}
         onClick={() => {
-          filterCategoryArray.length > 0 && handleSubmitClick();
+          filterCategoryArray.length > 0 && handleSubmitClick()
         }}
       >
         검색하기
       </S.DirectBtn>
     </S.CategorySectionWrapper>
-  );
-};
+  )
+}
 
-export default CategorySection;
+export default CategorySection
