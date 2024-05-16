148d9a36bbc8cee575e731a7fedc772b5421342d
yoohwanbin <hwanbin@infludeo.com>
2024-05-16 16:57:10 +0900

    Fix : next.config.js

--- next.config.js
+++ next.config.js
@@ -3,38 +3,44 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "www.notion.so",
-        pathname: "**",
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "s3.us-west-2.amazonaws.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "w.namu.la",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "i.namu.wiki",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'w.namu.la',
+        pathname: '**',
       },
       {
-        protocol: "https",
-        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
-        pathname: "**",
+        protocol: 'https',
+        hostname: 'i.namu.wiki',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        pathname: '**',
       },
     ],
   },
-  format: ["image/png", "images/webp", "image/jpeg"],
+  format: ['image/png', 'images/webp', 'image/jpeg'],
   reactStrictMode: true,
   plugins: {
     autoprefixer: {},
@@ -42,6 +48,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
