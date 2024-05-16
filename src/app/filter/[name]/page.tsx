7bdbcb5dc51784115f358055914b6c5400021e49
yoohwanbin <hwanbin@infludeo.com>
2024-05-16 17:55:59 +0900

    Fix : etc

--- src/app/filter/[name]/page.tsx
+++ src/app/filter/[name]/page.tsx
@@ -1,18 +1,18 @@
-import Home from "@/components/Home";
-import { listProps } from "@/types";
-import { Props } from "@/types/common";
-import { decodeParams } from "@/utils/decodeParams";
-import { Metadata } from "next";
-import { getFilterList } from "@/api/list";
+import Home from '@/components/Home'
+import { listProps } from '@/types'
+import { Props } from '@/types/common'
+import { decodeParams } from '@/utils/decodeParams'
+import { Metadata } from 'next'
+import { getFilterList } from '@/api/list'
 
 export const metadata: Metadata = {
-  title: "Filter",
-};
+  title: 'Filter',
+}
 
 export const FilterPage = async ({ params: { name } }: Props) => {
-  const list: listProps[] = await getFilterList(decodeParams(name));
+  const list: listProps[] = await getFilterList(decodeParams(name))
 
-  return <Home initList={list} />;
-};
+  // return <Home initList={list} />;
+}
 
-export default FilterPage;
+export default FilterPage
--- src/app/home/[name]/page.tsx
+++ src/app/home/[name]/page.tsx
@@ -1,6 +1,6 @@
 import Home from '@/components/Home'
 import { Props } from '@/types/common'
-import { WtmListType, getWtmData, getWtmFilterData } from 'wtm-api'
+import { WtmListType, getWtmData } from 'wtm-api'
 
 export default async function HomePage({ params: {} }: Props) {
   const list: WtmListType[] = await getWtmData()
