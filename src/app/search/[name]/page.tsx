a39d0fa8b7ffe008856fe0578e93fa14d83d1ac0
yoohwanbin <hwanbin@infludeo.com>
2024-05-16 17:36:43 +0900

    Refactor : Search page

--- src/app/search/[name]/page.tsx
+++ src/app/search/[name]/page.tsx
@@ -1,18 +1,17 @@
-import { listProps } from "@/types";
-import Home from "@/components/Home";
-import { Props } from "@/types/common";
-import { decodeParams } from "@/utils/decodeParams";
-import { Metadata } from "next";
-import { getSearchList } from "@/api/list";
+import Home from '@/components/Home'
+import { Props } from '@/types/common'
+import { decodeParams } from '@/utils/decodeParams'
+import { Metadata } from 'next'
+import { getWtmSearchData } from 'wtm-api'
 
 export const metadata: Metadata = {
-  title: "Search",
-};
+  title: 'Search',
+}
 
 export const SearchPage = async ({ params: { name } }: Props) => {
-  const list: listProps[] = await getSearchList(decodeParams(name));
+  const list = await getWtmSearchData(decodeParams(name))
 
-  return <Home initList={list} />;
-};
+  return <Home initList={list} />
+}
 
-export default SearchPage;
+export default SearchPage
